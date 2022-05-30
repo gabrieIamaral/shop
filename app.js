efcd01d381336b317868d7908a7819ca15f68cce
Gabriel Amaral <gabrielamaral.dev@gmail.com>
2022-05-30 19:56:04 -0300

    added cart interaction with js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 let searchBtn = document.getElementsByClassName('search-icon')[0];
 let searchBar = document.getElementsByClassName('search-icon')[1];
 let overflow = document.getElementById('body');
+let cartBtn = document.getElementsByClassName('cart-icon')[0];
+let cart = document.getElementsByClassName('cart-icon')[1];
+let closeBtn = document.getElementsByClassName('cart-icon')[2];
 
 function search() {
     searchBtn.classList.toggle('search-toggle');
@@ -9,4 +12,19 @@ function search() {
     overflow.classList.toggle('not-overlay');
 }
 
+function cartProducts() {
+    cartBtn.classList.toggle('cart-active');
+    cart.classList.toggle('cart-active');
+    overflow.classList.toggle('not-overlay');
+}
+
+function closeCart() {
+    closeBtn.classList.toggle('cart-close');
+    cartBtn.classList.toggle('cart-active');
+    cart.classList.toggle('cart-active');
+    overflow.classList.toggle('not-overlay');
+}
+
 searchBtn.addEventListener('click', search);
+cartBtn.addEventListener('click', cartProducts);
+closeBtn.addEventListener('click', closeCart);
